bfdd207d3fca47627c27ff49302a6383286f63af
brzoza <brzozowskic@gmail.com>
2016-02-25 11:46:51 +0100

    Fixing contact.php

--- contact.php
+++ contact.php
@@ -1,8 +1,15 @@
 <?php
-	// ========== Enter your email address here ========== //
-	$to = "YOUR EMAIL";
-	
-	// Clean up the input values
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ */
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require 'PHPMailerAutoload.php';
+
+// Clean up the input values
 	foreach($_POST as $key => $value) {
 		if(ini_get('magic_quotes_gpc'))
 			$_POST[$key] = stripslashes($_POST[$key]);
@@ -55,16 +62,70 @@
 	else {
 		$subject = "Contact Form: $name";
 	}
-	$message = "$message";
-	$headers = "From: ".$name." <".$email.">" . "\r\n" . "Reply-To: " . $email;
-	
-	mail($to, $subject, $message, $headers);
-	
-	// Output success message
-	die("<p class='success'>Thank you! – Wiadomość została wysłana!</p>");
-	
-	// Check if email is valid
-	function validEmail($email) {
+
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "alexwebdesigncontactform@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "1234qwerzxcv)(*&";
+
+//Set who the message is to be sent from
+$mail->setFrom($email, $name);
+
+//Set an alternative reply-to address
+$mail->addReplyTo($email, $name);
+
+//Set who the message is to be sent to
+$mail->addAddress('cezary@alexwebdesign.pl', 'Cezary Brzozowski');
+
+//Set the subject line
+$mail->Subject = $subject;
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML($message);
+//Replace the plain text body with one created manually
+$mail->AltBody = $message;
+
+
+//send the message, check for errors
+if (!$mail->send()) {
+    die("<p class='success'>Pojawił się bład. Spróbuj wysłać email na adres cezary@alexwebdesign.pl !</p>");
+} else {
+    die("<p class='success'>Dziękuję! – Wiadomość została wysłana!</p>");
+}
+function validEmail($email) {
 		$isValid = true;
 		$atIndex = strrpos($email, "@");
 		if (is_bool($atIndex) && !$atIndex)	{
